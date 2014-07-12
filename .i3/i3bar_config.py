d658546d9d29a922cc4cddcb3c9fa04a615df2de
emilecaron <emile.caron@outlook.com>
2014-07-12 04:19:05 +0200

    Unified colors and added ping module display

--- .i3/i3bar_config.py
+++ .i3/i3bar_config.py
@@ -5,60 +5,56 @@ import subprocess
 
 from i3pystatus.core import Status
 
-status = Status(standalone=True)
+status = Status(standalone=True, reversed_register=False)
 
+# Load (last minute)
+#status.register("load",
+#        critical_limit=0.5,
+#        format="load:{avg1}%",)
 
+#status.register("cpu_usage_bar")
 
-# Shows the average load of the last minute and the last 5 minutes
-# (the default value for format is used)
-status.register("load",
-        critical_limit=0.5,
-        format="load:{avg1}%",
-        
-        )
+# Ping
+status.register('ping')
 
+# CPU use
+status.register("cpu_usage")
 
 # Shows your CPU temperature, if you have a Intel CPU
 status.register("temp",
     format="{temp:.0f}°C",)
 
+# Mem use
+status.register("mem",
+        color="#ffffff")
 
-# Shows the address and up/down state of eth0. If it is up the address is shown in
-# green (the default value of color_up) and the CIDR-address is shown
-# (i.e. 10.10.10.42/24).
-# If it's down just the interface name (eth0) will be displayed in red
-# (defaults of format_down and color_down)
-#
-# Note: the network module requires PyPI package netifaces
+
+# Ethernet
 status.register("network",
     interface="enp3s0",
-    format_up="{v4cidr}",)
+    format_up="{v4cidr}",
+    color_up="#ffffff")
 
-# Has all the options of the normal network and adds some wireless specific things
-# like quality and network names.
-#
-# Note: requires both netifaces and basiciw
+# Wifi
 status.register("wireless",
     interface="wlp2s0",
-    format_up="{essid} {quality:03.0f}% [{v4}]",)
+    format_up="{essid} {quality:03.0f}% [{v4}]",
+    color_up="#ffffff")
 
-# Shows disk usage of /
-# Format:
-# 42/128G [86G]
+# Root disk usage
 status.register("disk",
     path="/",
     format="{used}/{total}Go",)
 
 # Shows pulseaudio default sink volume
 #
-# Note: requires libpulseaudio from PyPI
-status.register("pulseaudio",
-    format="♪{volume}",)
+# Note: requires libpulseaudio from PyPI 
+# Salesale Pypi demande --allow-unverified et --allow-external
+#status.register("pulseaudio",
+#    format="♪{volume}",)
 
 
-# Displays clock like this:
 # Tue 30 Jul 11:59:46 PM KW31
-#                          ^-- calendar week
 status.register("clock",
     format="%a %-d %b %X KW%V",)
 
